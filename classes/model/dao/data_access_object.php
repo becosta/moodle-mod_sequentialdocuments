f511fe99003bc844583aec5b407a731adedfd165
bcosta <be.costa75@gmail.com>
2014-11-14 16:07:10 +0100

    Implements classe data_access_object.

--- classes/model/dao/data_access_object.php
+++ classes/model/dao/data_access_object.php
@@ -26,49 +26,141 @@ defined('MOODLE_INTERNAL') || die();
 
 abstract class data_access_object {
 
-    protected $instanceid = 0;
+    protected $instanceid = -1;
 
-    abstract protected function get_getentity_method();
+    abstract protected function get_getentity_method($DB, $instanceid, $id);
 
-    abstract protected function get_getentitywhere_method();
+    abstract protected function get_getentitywhere_method($DB, $instanceid, array $conditions);
 
-    abstract protected function get_getallentitieswhere_method();
+    abstract protected function get_getallentitieswhere_method($DB, $instanceid, array $conditions);
 
-    abstract protected function get_insert_method();
+    abstract protected function get_insert_method($DB, $instanceid, $entity);
 
-    abstract protected function get_update_method();
+    abstract protected function get_update_method($DB, $instanceid, $entity);
 
-    abstract protected function get_delete_method();
+    abstract protected function get_delete_method($DB, $instanceid, $entity);
 
     public function __construct($instanceid) {
-        if (!is_int($instanceid) || !($instanceid > 0)) {
-            throw new InvalidArgumentException();
-        }
+        entity::check_numeric_id($id);
         $this->instanceid = $instanceid;
     }
 
     public function get_entity($id) {
+        entity::check_numeric_id($id);
 
+        global $DB;
+        try {
+
+            $lambda = $this->get_getentity_method();
+            $this->check_lambda_method($lambda);
+
+            $entity = $lambda($DB, $this->instanceid, $id);
+
+        } catch (dml_exception $e) {
+            return false;
+        }
+        return $entity;
     }
 
-    public function get_entity_where(array $condition) {
+    public function get_entity_where(array $conditions) {
+        if ($conditions === null) {
+            throw new InvalidArgumentException();
+        }
 
+        global $DB;
+        try {
+
+            $lambda = $this->get_getentitywhere_method();
+            $this->check_lambda_method($lambda);
+
+            $entity = $lambda($DB, $this->instanceid, $conditions);
+
+        } catch (dml_exception $e) {
+            return false;
+        }
+        return $entity;
     }
 
-    public function get_all_entities_where(array $condition) {
+    public function get_all_entities_where(array $conditions) {
+        if ($conditions === null) {
+            throw new InvalidArgumentException();
+        }
 
+        global $DB;
+        try {
+
+            $lambda = $this->get_getallentitieswhere_method();
+            $this->check_lambda_method($lambda);
+
+            $entities = $lambda($DB, $this->instanceid, $conditions);
+
+        } catch (dml_exception $e) {
+            return false;
+        }
+        return $entities;
     }
 
     public function insert(entity $entity) {
+        if ($entity === null) {
+            throw new InvalidArgumentException();
+        }
 
+        global $DB;
+        try {
+
+            $lambda = $this->get_insert_method();
+            $this->check_lambda_method($lambda);
+
+            return $lambda($DB, $this->instanceid, $entity);
+
+        } catch (dml_exception $e) {
+            return false;
+        }
     }
 
     public function update(entity $entity) {
+        if ($entity === null) {
+            throw new InvalidArgumentException();
+        }
 
+        global $DB;
+        try {
+
+            $lamda = $this->get_update_method();
+            $this->check_lambda_method($lambda);
+
+            return $lamda($DB, $this->instanceid, $entity);
+
+        } catch (dml_exception $e) {
+            return false;
+        }
     }
 
     public function delete(entity $entity) {
+        if ($entity === null) {
+            throw new InvalidArgumentException();
+        }
 
+        global $DB;
+        try {
+
+            $lambda = $this->get_delete_method();
+            $this->check_lambda_method($lambda);
+
+            return $lambda($DB, $this->instanceid, $entity);
+
+        } catch (dml_exception $e) {
+            return false;
+        }
+    }
+
+    protected function check_lambda_method($lambda) {
+        if (!is_callable($lambda)) {
+            throw new LogicException(
+                    'Bad data_access_object specialization, '.
+                    '"get_getentity_method" should return a lambda function'
+            );
+        }
     }
 }
 
